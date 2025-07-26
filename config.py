a682b46197496342eab097d48dc3a42954377801
abeeranajam31 <128603901+abeeranajam31@users.noreply.github.com>
2025-07-26 15:56:59 +0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+from sentence_transformers import SentenceTransformer
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+DB_NAME = "hybrid_retrieval"
+COLLECTION_NAME = "issues"
+
+HF_API_KEY = os.getenv("HF_API_KEY")
+HF_MODEL_ID = "sentence-transformers/paraphrase-MiniLM-L6-v2"
+HF_MODEL_URL = f"https://api-inference.huggingface.co/models/{HF_MODEL_ID}"
